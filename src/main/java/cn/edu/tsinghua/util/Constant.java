40195445608b9a776cdf97d0a9bf344deda39305
RedTea14 <zhngmatt@gmail.com>
2014-10-21 22:46:48 +0800

    Matt added.

--- src/main/java/cn/edu/tsinghua/util/Constant.java
+++ src/main/java/cn/edu/tsinghua/util/Constant.java
@@ -1,9 +1,17 @@
 package cn.edu.tsinghua.util;
 
-/**常量类
+/**甯搁噺绫�
  * Created by liulei on 2014/5/23.
  */
 public class Constant {
     public final static String OBJECT_SERIALIZATION_STORAGE_PATH = "storage/";
-
+    public final static String DATA_PATH = "data/";
+    public final static String FILE_SEPARATOR = "/";
+    public final static String WIN_FILE_SEPARATOR = "\\";
+    public final static String FILE_EXTENSION_SEPARATOR = ".";
+    public final static String PROJECT_FILE_EXTENSION = "stp";
+    public final static String BASE_FOLDER_NAME = "SoftwareTrust";
+    public final static int WINDOW_HEIGHT = 768;
+    public final static int WINDOW_WIDTH = 1024;
+    public final static int TOOL_PANEL_HEIGHT_RATIO = 10;
 }
